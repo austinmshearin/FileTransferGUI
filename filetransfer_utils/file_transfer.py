ff5a879f1af57e812e7999bcb09830267a0d6299
Austin Shearin <austinmshearin@gmail.com>
2023-06-21 13:47:19 -0500

    Testing assertion errors of the get_files function in file_transfer

--- filetransfer_utils/file_transfer.py
+++ filetransfer_utils/file_transfer.py
@@ -2,5 +2,32 @@
 Package for handling basic file transfer processes
 """
 # Standard Imports
+import os
 
 # Local Imports
+
+
+def get_files(filepath: str, include_file_extensions: list = [], exclude_file_extensions: list = []) -> tuple:
+    """
+    Retrieves all files within the filepath with specified file extension excluding any specified file extensions
+
+    Parameters
+    ----------
+    filepath: str
+        Filepath to retrieve files
+    include_file_extensions: list = [str]
+        File extensions to include in retrieval  
+        Will retrieve all if none specified  
+    exclude_file_extensions: list = [str]
+        File extensions to exclude in retrieval  
+        Will exclude none if none specified
+
+    Returns
+    -------
+    tuple
+        List of all filenames, list of all relative filepaths, list of all full filepaths
+    """
+    assert type(include_file_extensions) is list, "include_file_extensions must be a list"
+    assert type(exclude_file_extensions) is list, "exclude_file_extensions must be a list"
+    assert all([type(extension) is str for extension in include_file_extensions]), "File extentions must be strings"
+    assert all([type(extension) is str for extension in exclude_file_extensions]), "File extentions must be strings"
--- test_filetransfer_utils/test_filetransfer_utils.py
+++ test_filetransfer_utils/test_filetransfer_utils.py
@@ -2,6 +2,64 @@
 Unit testing of the file_transfer package
 """
 # Standard Imports
+import os
+import shutil
+import pytest
 
 # Local Imports
 import filetransfer_utils.file_transfer as file_transfer
+
+# Environment variables
+dummy_src = "Temp_src"
+dummy_des = "Temp_des"
+
+
+def setup_module():
+    """
+    Sets up a dummy source and destination folderpaths for unit testing
+    """
+    # Prepare source and destination filepaths
+    os.makedirs(dummy_src, exist_ok=True)
+    os.makedirs(dummy_des, exist_ok=True)
+
+
+def teardown_module():
+    """
+    Tears down the dummy source and destination folderpaths for unit testing
+    """
+    # Delete all temporary files and folders from source and destination filepaths
+    shutil.rmtree(dummy_src)
+    shutil.rmtree(dummy_des)
+
+
+class TestGetFilesAssertions:
+    """
+    Tests that get files method returns assertion errors for edge cases
+    """
+    def test_include_extension_not_list(self):
+        with pytest.raises(AssertionError):
+            _ = file_transfer.get_files(
+                filepath=dummy_src,
+                include_file_extensions="A"
+            )
+
+    def test_exclude_extension_not_list(self):
+        with pytest.raises(AssertionError):
+            _ = file_transfer.get_files(
+                filepath=dummy_src,
+                exclude_file_extensions="A"
+            )
+
+    def test_include_extension_contains_not_str(self):
+        with pytest.raises(AssertionError):
+            _ = file_transfer.get_files(
+                filepath=dummy_src,
+                include_file_extensions=["A", 1]
+            )
+
+    def test_exclude_extensions_contains_not_str(self):
+        with pytest.raises(AssertionError):
+            _ = file_transfer.get_files(
+                filepath=dummy_src,
+                exclude_file_extensions=["A", 1]
+            )
